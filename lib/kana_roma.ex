3a2db64533adb70dabd7aa7c3aa730f8d5bd9a59
aratakokubun <usinghtc@gmail.com>
2018-12-31 00:15:54 +0900

    Refactor based on credo result

--- lib/kana_roma.ex
+++ lib/kana_roma.ex
@@ -3,8 +3,8 @@ defmodule KanaRoma do
   Module to convert Japanese characters between kana-moji('かな') and roman-moji('kana').
   """
 
-  alias Models.KanaRomaTable, as: Table
   alias Models.KanaRomaPair, as: Pair
+  alias Models.KanaRomaTable, as: Table
 
   @max_kana_len 2
   @max_roma_len 3
@@ -19,37 +19,44 @@ defmodule KanaRoma do
       iex> KanaRoma.kana2roma "あいう azAZ09% ａｚＡＺ０９、。 わをん"
       "aiu azAZ09% ａｚＡＺ０９、。 wawon"
   """
+  @spec kana2roma(String) :: String
   def kana2roma(kana) do
-    kana2roma(kana, 0, @max_kana_len, [])
+    kana
+    |> kana2roma(0, @max_kana_len, [])
     |> Enum.map(&(&1.roma))
     |> Enum.join()
   end
 
+  @spec kana2roma(String, Integer, Integer, [Pair.pair]) :: [Pair.pair]
   def kana2roma(kana, index, length, converted) do
-    cond do
-      String.length(kana)-1 < index -> converted
-      true                          ->
-        with sliced_kana <- String.slice(kana, index..index+length) do
-          Table.get_kana_table(length)
-          |> Enum.find(&(&1.kana == sliced_kana))
-          |> append_sliced_kana(kana, index, length, sliced_kana, converted)
-        end
+    if String.length(kana) - 1 < index do
+      converted
+    else
+      with sliced_kana <- String.slice(kana, index..index + length) do
+        length
+        |> Table.get_kana_table
+        |> Enum.find(&(&1.kana == sliced_kana))
+        |> append_sliced_kana(kana, index, length, sliced_kana, converted)
+      end
     end
   end
 
+  @spec append_sliced_kana(Pair.pair, String, Integer, Integer, String, [Pair.pair]) :: [Pair.pair]
   defp append_sliced_kana(nil, kana, index, 0, sliced_kana, converted) do
     with appended <- converted ++ [%Pair{kana: sliced_kana, roma: sliced_kana}] do
-      kana2roma(kana, index+1, @max_kana_len, appended)
+      kana2roma(kana, index + 1, @max_kana_len, appended)
     end
   end
 
+  @spec append_sliced_kana(Pair.pair, String, Integer, Integer, String, [Pair.pair]) :: [Pair.pair]
   defp append_sliced_kana(nil, kana, index, length, _sliced_kana, converted) do
-    kana2roma(kana, index, length-1, converted)
+    kana2roma(kana, index, length - 1, converted)
   end
 
+  @spec append_sliced_kana(Pair.pair, String, Integer, Integer, String, [Pair.pair]) :: [Pair.pair]
   defp append_sliced_kana(pair, kana, index, length, _sliced_kana, converted) do
     with appended <- converted ++ [pair] do
-      kana2roma(kana, index+1+length, @max_kana_len, appended)
+      kana2roma(kana, index + 1 + length, @max_kana_len, appended)
     end
   end
 
@@ -63,37 +70,43 @@ defmodule KanaRoma do
       iex> KanaRoma.roma2kana "aiu azAZ09% ａｚＡＺ０９、。 wawon"
       "あいう あzAZ09% ａｚＡＺ０９、。 わをん"
   """
+  @spec roma2kana(String) :: String
   def roma2kana(roma) do
-    roma2kana(roma, 0, @max_roma_len, [])
+    roma
+    |> roma2kana(0, @max_roma_len, [])
     |> Enum.map(&(&1.kana))
     |> Enum.join()
   end
 
+  @spec roma2kana(String, Integer, Integer, [Pair.pair]) :: [Pair.pair]
   def roma2kana(roma, index, length, converted) do
-    cond do
-      String.length(roma)-1 < index -> converted
-      true                          ->
-        with sliced_roma <- String.slice(roma, index..index+length) do
-          Table.get_combined_table
-          |> Enum.find(&(&1.roma == sliced_roma))
-          |> append_sliced_roma(roma, index, length, sliced_roma, converted)
-        end
+    if String.length(roma) - 1 < index do
+      converted
+    else
+      with sliced_roma <- String.slice(roma, index..index + length) do
+        Table.get_combined_table
+        |> Enum.find(&(&1.roma == sliced_roma))
+        |> append_sliced_roma(roma, index, length, sliced_roma, converted)
+      end
     end
   end
 
+  @spec append_sliced_roma(Pair.pair, String, Integer, Integer, String, [Pair.pair]) :: [Pair.pair]
   defp append_sliced_roma(nil, roma, index, 0, sliced_roma, converted) do
     with appended <- converted ++ [%Pair{kana: sliced_roma, roma: sliced_roma}] do
-      roma2kana(roma, index+1, @max_roma_len, appended)
+      roma2kana(roma, index + 1, @max_roma_len, appended)
     end
   end
 
+  @spec append_sliced_roma(Pair.pair, String, Integer, Integer, String, [Pair.pair]) :: [Pair.pair]
   defp append_sliced_roma(nil, roma, index, length, _sliced_roma, converted) do
-    roma2kana(roma, index, length-1, converted)
+    roma2kana(roma, index, length - 1, converted)
   end
 
+  @spec append_sliced_roma(Pair.pair, String, Integer, Integer, String, [Pair.pair]) :: [Pair.pair]
   defp append_sliced_roma(pair, roma, index, length, _sliced_roma, converted) do
     with appended <- converted ++ [pair] do
-      roma2kana(roma, index+1+length, @max_roma_len, appended)
+      roma2kana(roma, index + 1 + length, @max_roma_len, appended)
     end
   end
 end
--- lib/models/kana_roma_pair.ex
+++ lib/models/kana_roma_pair.ex
@@ -1,3 +1,12 @@
 defmodule Models.KanaRomaPair do
-  defstruct roma: 'roma', kana: 'かな'
+  @moduledoc """
+  Struct for a pair of corresponding kana and roman characters.
+  """
+
+  defstruct kana: 'かな', roma: 'roma'
+
+  @typedoc """
+      Type that represents KanaRomaPair struct with :kana as String and :roma as String.
+  """
+  @type pair :: %Models.KanaRomaPair{kana: String, roma: String}
 end
